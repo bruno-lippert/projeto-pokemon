38416f0a6b0b14fc4c6f2d78191fcdf3a94a07ed
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-08-01 22:13:15 -0300

    criação da função do botão voltar

--- files/js/index.js
+++ files/js/index.js
@@ -6,5 +6,17 @@ function forwardButton(){
     cards[currenCard-1].classList.remove("selected")
 }
 
+function backButton() {
+    const cards = document.querySelectorAll(".card")
+
+    currenCard--
+    
+    if(currenCard <= 0){
+        currenCard = 0
+    }
+    
+    cards[currenCard].classList.add("selected")
+    cards[currenCard+1].classList.remove("selected")
+}
 
 let currenCard = 0
