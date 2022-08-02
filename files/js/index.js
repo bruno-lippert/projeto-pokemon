07cc08bfadd6b54d72a4a53038760faa0875108a
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-08-01 22:01:40 -0300

    função do botão avançar concluído

--- files/js/index.js
+++ files/js/index.js
@@ -1,8 +1,8 @@
 function forwardButton(){
     const cards = document.querySelectorAll(".card")
-    let currenCard = 0
-
+    
     currenCard++
     cards[currenCard].classList.add("selected")
     cards[currenCard-1].classList.remove("selected")
 }
+let currenCard = 0
